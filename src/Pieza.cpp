436992472c9ee71355b621084e57c18ffc9a13a2
JunjieDai2003 <162756402+JunjieDai2003@users.noreply.github.com>
2025-05-19 18:29:59 +0800

    quite la herencia de casilla en pieza

--- src/Pieza.cpp
+++ src/Pieza.cpp
@@ -2,13 +2,6 @@
 #include "ETSIDI.h"
 Pieza::Pieza()
 {
-	movida = 0;
-}
-Pieza::Pieza(const Pieza& p)
-{
-	tipo = p.tipo;
-	color = p.color;
-	casilla = p.casilla;
 }
 
 Pieza::~Pieza() {}
--- src/Pieza.h
+++ src/Pieza.h
@@ -5,7 +5,7 @@
 enum  Tipo { VACIO, PEON, CABALLO, ALFIL, TORRE, REY, REINA };
 enum Color { NO_COLOR=0, BLANCO, NEGRO };
 
-class Pieza :public Casilla
+class Pieza 
 {
 	//Hago :public Casilla porque todas las piezas contienen
 	//una casilla asi puedes usar su constructor
@@ -14,19 +14,15 @@ protected: //Lo hago protected porque de pieza van a heredar otras clases
 	Color color;
 	Casilla casilla;
 	int fila, columna;
-	bool movida = 0;//Para ver si la pieza se ha movido durante la partida, lo veremos proximamente
+	
 	//Todas las piezas tienen un color, son un tipo,
 	//y están en una Casilla
 	//Inicializo movida en 0, siempre presuponemos que no se mueve VER funcion puedeMoverse()
 public:
 	//CONSTRUCTORES
-	Pieza(Tipo tipo, Color color, int fila, int col) :tipo(tipo), color(color),  fila(fila), columna(col) {};
 	Pieza(Tipo tipo, Color color, Casilla casilla) :tipo(tipo), color(color), casilla(casilla){};
 	Pieza(); //CONSTRUCTOR por defecto
-	Pieza(/*int fila, int columna,*/  Casilla cas, Color color, Tipo tipo) :/*fila(fila), columna(columna),*/  Casilla(cas), color(color), tipo(tipo) {}
-	//Porque crea fila y columna en vez de meter directamente la Casilla??? asi se ahorra crear y aprovecha lo creado???
-	//MODIFICAR EL CONSTRUCTOR
-	Pieza(const Pieza& p); //CONSTRUCTOR PUEDE COPIAR A OTRA PIEZA?????
+	
 	//DESTRUCTOR por defecto
 	~Pieza();
 	//////////////////////////////////////////////////////////////////////////////////////////
@@ -37,7 +33,7 @@ public:
 	Tipo getTipo()const { return tipo; }
 	Color getColor()const { return color; }
 	Casilla getCasilla()const { return casilla; }
-	bool getMovida()const { return movida; }//TODAVIA NO SE PARA QUE SIRVE NI COMO SE UTILIZA
+	
 	int getFila()const { return casilla.fila; }
 	int getColumna()const { return casilla.columna; }
 
@@ -48,7 +44,7 @@ public:
 	//me gustaria averiguar esto
 	//void setCasilla(Casilla casilla) { casilla.fila =fila; casilla.columna = columna; }
 	void setCasilla(int fil, int colum) { casilla.fila = fil; casilla.columna = colum; }
-	void setMovida(bool a) { movida = a; }//ESTO NO SIRVE??? 
+	
 	//No hace falta settear la fila y columna porque en nuestro caso usamos directamente
 	//la casilla que creo que es mejor, ya veremos si lo cambiamos o que 
 
